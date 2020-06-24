26aa062474317da8dc0f00183292fb0f2e299a4d
Wisnu <wisnudentadio@gmail.com>
2020-06-24 09:29:32 +0700

    add atlas mongodb connection

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,11 +1,17 @@
-if(process.env.NODE_ENV === 'production') {
-    module.exports = {
-        mongoURI: "mongodb+srv://wisnudm:1234@cluster0-liiwc.mongodb.net/easypcassemble?retryWrites=true&w=majority",
-        secret: 'secret'
-    };
-} else {
-    module.exports = {
-        mongoURI: "mongodb://localhost:27017/easypcassemble",
-        secret: 'secret'
-    };
-}
+// if(process.env.NODE_ENV === 'production') {
+//     module.exports = {
+//         mongoURI: "mongodb+srv://wisnudm:1234@cluster0-liiwc.mongodb.net/easypcassemble?retryWrites=true&w=majority",
+//         secret: 'secret'
+//     };
+// } else {
+//     module.exports = {
+//         mongoURI: "mongodb://localhost:27017/easypcassemble",
+//         secret: 'secret'
+//     };
+// }
+
+
+module.exports = {
+    mongoURI: "mongodb+srv://wisnudm:1234@cluster0-liiwc.mongodb.net/easypcassemble?retryWrites=true&w=majority",
+    secret: 'secret'
+};
